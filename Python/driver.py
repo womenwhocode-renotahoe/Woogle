fcf9a222611182e6d092ab6d7c2f1de8a488e129
Erin Keith <erinkeith.unr@gmail.com>
2014-11-30 13:12:44 -0800

    Update driver.py

--- Python/driver.py
+++ Python/driver.py
@@ -1,1 +1,5 @@
-print "Hello World"
+"""
+This file is to be used as the main driver for the webscraping program.
+Any additional functions created in other modules should be imported
+and used here to streamline the webscraping process.
+"""
