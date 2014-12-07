bbb77f60524243969f655f6aa0c77ae6adec3c71
Erin Keith <erinkeith.unr@gmail.com>
2014-12-07 11:30:10 -0800

    removing to test instructions

--- Learning-Material/TuringTest/Erin/Conversation.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'tabby'
-import Responses
-
-response = ''
-
-while not Responses.is_farewell(response):
-    response = raw_input('Your turn: ')
-    response = response.lower()
-    print Responses.respond(response)
