919d290fceaf42732bcce681764038e952d66663
Erin Keith <erinkeith.unr@gmail.com>
2014-12-07 12:34:21 -0800

    remove files

--- Learning-Material/TuringTest/Conversation.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'tabby'
-from Responses import respond, is_farewell
-
-response = ''
-
-while not is_farewell(response):
-    response = raw_input('Your turn: ')
-    response = response.lower()
-    print respond(response)
