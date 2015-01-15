61406c1d8ee56fbde09d458f974b9b5b00c239f5
Britni <me@britnidesigns.com>
2015-01-14 19:11:32 -0800

    Basic function for loading a text file of keywords

--- Python/load_keywords_and_URL_list.py
+++ Python/load_keywords_and_URL_list.py
@@ -3,3 +3,11 @@ This file is to contain a function for loading the source URL list and search ke
 input:          filename
 output:         data structures containing URL list and search keywords list
 '''
+
+def loadSourceList(filename):
+	textFile = open(filename, 'r')
+	textString = textFile.read()
+	sourceList = textString.split('\n')
+	print sourceList
+
+loadSourceList('keywords.txt')
