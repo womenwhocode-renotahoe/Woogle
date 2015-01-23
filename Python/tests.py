3b39961710f2e1ba27c1146cb9c1f5bd7df33b7c
Jenny Steele <jenny@jennyscript.com>
2015-01-22 19:39:30 -0800

    Add tests for parse web content. Also add requirements.txt file to run like "pip install -r requirements.txt" for dependencies.

---
+++ Python/tests.py
@@ -0,0 +1,11 @@
+from nose.tools import assert_equals
+
+from parse_web_content import parse_content
+
+class TestParseWebContent:
+    def test_true_if_has_keyword(self):
+        assert_equals(parse_content(['red'], 'got red'), True)
+    def test_false_if_doesnt_have_keyword(self):
+        assert_equals(parse_content(['red'], 'got blue'), False)
+    def test_multiple_keywords(self):
+        assert_equals(parse_content(['red','yellow'], 'got yellow'), True)
