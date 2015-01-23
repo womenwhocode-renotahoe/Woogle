576c2d4909ec9a48ac2e41c46af8ecee018e50ac
Jenny Steele <jenny@jennyscript.com>
2015-01-22 18:45:57 -0800

    Add parse web content

--- Python/parse_web_content.py
+++ Python/parse_web_content.py
@@ -3,3 +3,9 @@ This file should contain a function which determines whether the fetched web con
 inputs:         search keyword list, webpage content
 output:         good or not good
 '''
+
+def parse_content(keywords, content):
+    if any(word in content for word in keywords):
+        return True
+    else:
+        return False
