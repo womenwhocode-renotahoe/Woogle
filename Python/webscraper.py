a8b63ade15a2fc2fc01ab72146d8f0e3dae84080
Jenny Steele <jenny@jennyscript.com>
2015-03-01 14:13:17 -0800

    Change file errors to raise OSErrors

--- Python/webscraper.py
+++ Python/webscraper.py
@@ -3,34 +3,20 @@ This file is to contain the driving program flow for the entire program per the 
 input:          the name of a configuration file containing a list of source URLs and a list of keywords for searching
 output:         (to file) a list of article titles and their corresponding URLs
 '''
+import os.path
 
 from load_keywords_and_URL_list import load_source_list
 
 source_URLs_file = 'urls.txt'
 search_keywords_file = 'keywords.txt'
 
-def check_source_file_exists(filename):
-	try:
-		f = open(filename)
-		f.close()
-		return True
-	except IOError as e:
-		return False	
-
 def check_file_errors(filename):
- 	if not check_source_file_exists(filename):
-		# TO-DO: raise error
-		return True
+ 	return not os.path.isfile(filename) or len(load_source_list(filename)) == 0
 
-	if len(load_source_list(filename)) = 0:
-		# TO-DO: raise error
-		return True
-	else:
-		return False
-
-def webscraper():
-	if check_file_errors(source_URLs_file) or check_file_errors(search_keywords_file):
-		# TO-DO: raise error
-		return
+def webscraper(source_file, keywords_file):
+	if check_file_errors(source_file):
+		raise OSError("Source file is not valid")
+	if check_file_errors(keywords_file):
+		raise OSError("Keywords file is not valid")
 
 	# continue ...
